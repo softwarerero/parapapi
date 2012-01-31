e7044e5164c598f4825dcf4cf2f39f031523211f
sun <sun@suncom.de>
2012-01-31 16:18:52 -0300

    design change

--- app/models/Category.java
+++ app/models/Category.java
@@ -12,31 +12,31 @@ import java.util.*;
  */
 public class Category {
 
-  public static final String[] main = {"transport", "realEstate", "household", "life",
-    "leisure", "electronics", "service", "job",
+  public static final String[] main = {"transport", "realEstate", "electronics", "household", "life",
+    "leisure", "service", "job",
     "farming", "business", "event", "contact", "education", "other" };
-  public static final String[] transport = {"auto", "bike", "bicycle", "water", "autoparts", "airplane"};
-  public static final String[] realEstate = {"house", "apartment", "twinHouse", "room", "commercial", "rural"};
-  public static final String[] household = {"furniture", "antique", "appliance"};
-  public static final String[] life = {"health", "wear", "cosmetic", "gift", "toy", "baby", "nutrition"};
-  public static final String[] leisure = {"home", "garden", "foto", "optic", "sport", "hunt", "fishery", "musicalInstrument"};
-  public static final String[] electronics = {"computer", "notebook", "software", "printer", "supply", "phone", "tv", "video"};
-  public static final String[] service = {"healthservice", "workshop", "finance", "transportservice", "construction", "animation", "eventTechnology", "multimedia", "security", "tourism"};
-  public static final String[] job = {"healthjob", "educationjob", "industry", "office", "householdjob"};
-  public static final String[] farming = {"equipment", "plant", "animal"};
-  public static final String[] business = {"machine", "officefurniture", "officesupply"};
-  public static final String[] event = {"fair", "party", "cinema", "theatre", "concert", "sportevent"};
-  public static final String[] contact = {"friendship", "venture", "club", "sheSeeksHim", "heSeeksHer"};
-  public static final String[] education = {"it", "academics", "sportcourse", "languages", "art", "driving", "book"};
+  public static final String[] transport = {"auto", "autoparts", "airplane", "bicycle", "bike", "water" };
+  public static final String[] realEstate = {"house", "commercial", "apartment", "twinHouse", "room", "rural"};
+  public static final String[] electronics = {"supply", "computer", "printer", "notebook", "software", "phone", "tv", "video"};
+  public static final String[] household = {"antique", "appliance", "furniture" };
+  public static final String[] life = {"baby", "cosmetic", "toy", "nutrition","gift", "wear", "health"};
+  public static final String[] leisure = {"hunt", "sport", "foto", "home", "musicalInstrument", "garden", "optic", "fishery"};
+  public static final String[] service = {"animation", "construction", "eventTechnology", "finance", "multimedia", "healthservice", "security", "workshop", "transportservice", "tourism"};
+  public static final String[] job = {"educationjob", "householdjob", "industry", "office", "healthjob"};
+  public static final String[] farming = {"animal", "equipment", "plant"};
+  public static final String[] business = {"officesupply", "machine", "officefurniture"};
+  public static final String[] event = {"cinema", "concert", "sportevent", "fair", "party", "theatre"};
+  public static final String[] contact = {"friendship", "venture", "club", "heSeeksHer", "sheSeeksHim"};
+  public static final String[] education = {"art", "sportcourse", "languages", "it", "book", "driving", "academics"};
   public static final String[] other = {"collection", "pet"};
 
   public static final Map<String, String[]> all = new HashMap() {{
     put("transport", transport);
     put("realEstate", realEstate);
+    put("electronics", electronics);
     put("household", household);
     put("life", life);
     put("leisure", leisure);
-    put("electronics", electronics);
     put("service", service);
     put("job", job);
     put("farming", farming);
